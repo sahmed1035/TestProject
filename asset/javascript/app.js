cac0a0bbdec083c67a3d467cb004f59160c4918f
sahmed <sahmed@alumni.depaul.edu>
2018-11-20 19:32:42 -0600

    typed.js working but needs fixing by syeda

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,13 +1,30 @@
 
 //Typing effect
-document.addEventListener('DOMContentLoaded', function(){
-    Typed.new('.element', {
-        strings: ["First sentence.", "Second sentence."],
-        typeSpeed: 50,
-        backSpeed: 50
-    });
-});
+var typed = new Typed('.element', {
+    strings: ["First sentence.", "Second sentence."],
+    typeSpeed: 30
+  });
 
+  
+
+  var typed = new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 0,
+    backSpeed: 0,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: false,
+    onComplete: function(self) { prettyLog('onCmplete ' + self) },
+    preStringTyped: function(pos, self) { prettyLog('preStringTyped ' + pos + ' ' + self); },
+    onStringTyped: function(pos, self) { prettyLog('onStringTyped ' + pos + ' ' + self) },
+    onLastStringBackspaced: function(self) { prettyLog('onLastStringBackspaced ' + self) },
+    onTypingPaused: function(pos, self) { prettyLog('onTypingPaused ' + pos + ' ' + self) },
+    onTypingResumed: function(pos, self) { prettyLog('onTypingResumed ' + pos + ' ' + self) },
+    onReset: function(self) { prettyLog('onReset ' + self) },
+    onStop: function(pos, self) { prettyLog('onStop ' + pos + ' ' + self) },
+    onStart: function(pos, self) { prettyLog('onStart ' + pos + ' ' + self) },
+    onDestroy: function(self) { prettyLog('onDestroy ' + self) }
+  });
 
 
 
